e5a9dc7a539ee08d3ed700d57dcdf2b8f5ce8be4
Richard Verdera, Jr <rverdera@pcso.gov.ph>
2022-05-26 13:56:38 +0800

    Initial Project

---
+++ pcso-rverdera/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace pcso_rverdera;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ pcso-rverdera/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace pcso_rverdera;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
